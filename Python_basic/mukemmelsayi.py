12c0f9d04bcd3cca553fe789e3e1f42a98150e24
Caglar <ahmetcmirza@gmail.com>
2021-04-23 14:39:42 +0200

    mukemmel ve polidromik sayi bulma

--- Python_basic/mukemmelsayi.py
+++ Python_basic/mukemmelsayi.py
@@ -1,4 +1,5 @@
 def muk_sayi(sayi):
+
     toplam = 0 
     sayac = 1
     while (sayac < sayi / 2 + 1):
@@ -10,7 +11,17 @@ def muk_sayi(sayi):
     else:
         return False
 
+def polid_sayi(sayi):
+    if str(sayi) == str(sayi)[::-1]:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
-    for i in range(2, 1000):
+    for i in range(2, 2000):
         if muk_sayi(i):
             print(i, "sayisi mukemmel sayidir.")
+        if polid_sayi(i):
+            if i > 1000:
+                print(i, "sayisi polidromik sayidir.")
