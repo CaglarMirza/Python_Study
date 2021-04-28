b88aafda727d04eaee44264dd0032e33f4a6581a
Caglar <ahmetcmirza@gmail.com>
2021-04-28 16:00:12 +0200

    Def ile (ucuz) asla sayi bulma

---
+++ Python_basic/ucuz (asal) bulma.py
@@ -0,0 +1,20 @@
+def asal(sayi):
+    sayac = 2
+    while sayac <sayi //2 +1:
+        if sayi % sayac == 0:
+            return False
+            sayac += 1
+        return True
+
+
+def ucuz_sayi(sayi):
+    if asal(sayi):
+        if (asal(sayi) and asal(sayi+2) and asal(sayi+6)) or (asal(sayi) and asal(sayi+4) and asal(sayi+6)):
+            print("sayi ucuz asal sayidir.")
+        else:
+            print("sayi ucuz asal sayi degildir.")
+    else:
+        print("gonderilen sayi asal sayi degildir")
+
+if __name__ == "__main__":
+    ucuz_sayi(17)
