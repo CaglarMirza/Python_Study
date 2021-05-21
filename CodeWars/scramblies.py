af2d3c2b6a9b9c7c274b756c57dc9eb234a1a131
Caglar <ahmetcmirza@gmail.com>
2021-05-21 13:14:21 +0200

    update

--- CodeWars/scramblies.py
+++ CodeWars/scramblies.py
@@ -1,12 +1,23 @@
 def scramble(s1, s2):
+    return all([True if s2.count(i)<=s1.count(i) else False for i in s2])
+
+
+    ###########-----------------
+
+
+
+
+def scramble(s1, s2):
     newlist = []
+    denem = list(s1)
     for i in list(s2):
-        if i in  list(s1):
+        if i in  denem:
             newlist.append(i)
+            denem.remove(i)
     if newlist==list(s2):
         return True
     else:
         return False
 
 
-print(scramble('cubtvzhetokfsd', 'bohzsdcetvtfuk'))
+print(scramble('', ''))
